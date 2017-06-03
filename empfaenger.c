96bd091c5c823c16b85af0d29b5488e48a7d352f
Michael Käfer <ic16b077@technikum-wien.at>
2017-06-03 19:13:58 +0200

    doxygen kommentare

--- empfaenger.c
+++ empfaenger.c
@@ -1,5 +1,48 @@
+/**
+ * @file empfaenger.c
+ * Betriebssysteme myipc File.
+ * Beispiel 3
+ *
+ * @author Aleksandar Pavlovic, Johanna Hoffmann, Michael Käfer
+ * @date 2017/06/03
+ *
+ * @version 1
+ *
+ * @todo Review
+ *
+ */
+
+/*
+ * -------------------------------------------------------------- includes --
+ */
 #include "myipclib.h"
 
+/*
+ * --------------------------------------------------------------- defines --
+ */
+
+/*
+ * ------------------------------------------------------------- functions --
+ */
+
+/*
+ * --------------------------------------------------------------- globals --
+ */
+
+/**
+ *
+ * \brief Implementaion of a receiver program
+ *
+ * This is the main entry point for any C program.
+ *
+ * \param argc the number of arguments
+ * \param argv the arguments itselves (including the program name in argv[0])
+ *
+ * \return success or failure
+ * \retval EXIT_SUCCESS success
+ * \retval EXIT_FAILURE failure
+ *
+ */
 int main(int argc, char* argv[]) {
 	int ringbuffer_size;
 
@@ -7,5 +50,9 @@ int main(int argc, char* argv[]) {
 
         printf("%d\n", ringbuffer_size);
 
-	return 0;
+	return EXIT_SUCCESS;
 }
+
+/*
+ * =================================================================== eof ==
+ */
--- sender.c
+++ sender.c
@@ -1,12 +1,58 @@
+/**
+ * @file sender.c
+ * Betriebssysteme myipc File.
+ * Beispiel 3
+ *
+ * @author Aleksandar Pavlovic, Johanna Hoffmann, Michael Käfer
+ * @date 2017/06/03
+ *
+ * @version 1
+ *
+ * @todo Review
+ *
+ */
+
+/*
+ * -------------------------------------------------------------- includes --
+ */
 #include "myipclib.h"
 
+/*
+ * --------------------------------------------------------------- defines --
+ */
+
+/*
+ * ------------------------------------------------------------- functions --
+ */
+
+/*
+ * --------------------------------------------------------------- globals --
+ */
+
+/**
+ *
+ * \brief Implementaion of a receiver program
+ *
+ * This is the main entry point for any C program.
+ *
+ * \param argc the number of arguments
+ * \param argv the arguments itselves (including the program name in argv[0])
+ *
+ * \return success or failure
+ * \retval EXIT_SUCCESS success
+ * \retval EXIT_FAILURE failure
+ *
+ */
 int main(int argc, char* argv[]) {
-        int ringbuffer_size;
+	int ringbuffer_size;
 
-        ringbuffer_size = get_ringbuffer_size(argc, argv);
+	ringbuffer_size = get_ringbuffer_size(argc, argv);
 
-	printf("%d\n", ringbuffer_size);
+        printf("%d\n", ringbuffer_size);
 
-        return 0;
+	return EXIT_SUCCESS;
 }
 
+/*
+ * =================================================================== eof ==
+ */
